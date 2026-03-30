51e74c65c0a9901d0bf9198a788f122566b6aa10
arjunsan22 <arjunanil2114@gmail.com>
2026-03-30 12:17:57 +0530

    bug fixed: in the feed page

--- app/page.jsx
+++ app/page.jsx
@@ -2,9 +2,5 @@ import Feeds from "@/app/(user)/feeds/page";
 import Home from "@/app/components/user/home";
 
 export default function Page() {
-  return (
-    <div className="p-6 bg-white rounded-lg shadow-md">
-      <Feeds />
-    </div>
-  );
+  return <Feeds />;
 }
